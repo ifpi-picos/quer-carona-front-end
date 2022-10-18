e7d630a5c4d008b4b0611fe1baa43fe625dadd69
WisleyASousa <wisleya.sousa@outlook.com.br>
2022-10-18 14:25:23 -0300

    reestruturando todos os arquivos, reolocando todas as pastas, organizando projeto por inteiro

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,23 @@
 const path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.ts',
-    module: {
-        rules: [
-        {
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            exclude: /node_modules/,
-            options: {
-                configFile: 'tsconfig.frontend.json',
-            }
-        },
-        ],
-    },
-    resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
-    },
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'frontend', 'assets', 'js'),
-    },
-    devtool: 'source-map',
-    };
+  entry: {
+    index: ['./src/index.ts']
+},
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'public', 'dist'),
+  },
+};
